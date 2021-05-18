069eb6ea95469763edccbec72fc4ca35a06187b9
vladyslav-semykin <zzzz_cccv@outlook.com>
2021-05-18 12:20:53 +0300

    start-js

---
+++ src/index.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const SKY_COLOR = "blue";
+
+// const a = 5;
+// const b = 6;
+// const x = a + b;
+
+// console.log("x:", x);
+
+// function hello() {
+//   console.log("hello world");
+// }
+
+// hello();
+// hello();
+
+// camel case - формат написания
+// const userName = "Bobby";
+// const age = 15;
+// const adult = false;
+// const eyeColor = "blue";
+
+// let g = 1;
+// console.log("g:", g);
+
+// g = 2;
+// console.log("g:", g);
+
+// g = 3;
+// console.log("g:", g);
+
+// =================== Обращение к переменной ========================
+
+// let y;
+// console.log("y:", y);
+
+// y = 6;
+// console.log("y:", y);
+
+// =================== Всплытие переменных ========================
+
+// console.log("t:", t);
+// let t = 11;
+
+// console.log("s:", s);
+// var s = 22;
+// console.log("s:", s);
+
+// =================== Получение данных - confirm ========================
+
+// const goToTheMovie = confirm("Please confirm go to the movie");
+// console.log("goToTheMovie:", goToTheMovie);
+
+// =================== Получение данных - prompt ========================
+const age = prompt("How old are you?");
+console.log("age:", age);
+
+if (age >= 18) {
+  console.log("you can buy ticket to this movie");
+} else {
+  console.log("you CAN`T go to this movie");
+}
